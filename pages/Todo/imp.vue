975dffc82853f3746c04bac007a99fb8dbb1ff14
Syed Hassan Raza <105318011+HassanRaza375@users.noreply.github.com>
2023-08-01 17:06:15 +0500

    updates

---
+++ pages/Todo/imp.vue
@@ -0,0 +1,23 @@
+<form action="/action_page.php">
+  <input type="file" id="myFile" name="filename">
+  <input type="submit">
+</form>
+
+
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+  border: 1px solid #ddd;
+}
+
+th, td {
+  text-align: left;
+  padding: 8px 3px;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+https://www.w3schools.com/howto/howto_css_table_zebra.asp
